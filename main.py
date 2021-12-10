8f3c569caefbe89315da73b9c74a2f96411de64b
jakub.buczek <jakub.buczek@skni.umcs.pl>
2021-12-10 20:24:44 +0100

    Starting of omemo implementation, updated .gitignore

--- main.py
+++ main.py
@@ -7,6 +7,11 @@ import asyncio
 from slixmpp import ClientXMPP
 from slixmpp.exceptions import IqError, IqTimeout
 
+import slixmpp_omemo
+from slixmpp_omemo import PluginCouldNotLoad, MissingOwnKey, EncryptionPrepareException
+from slixmpp_omemo import UndecidedException, UntrustedException, NoAvailableSession
+
+
 import sys
 
 if sys.platform == 'win32':
@@ -60,7 +65,69 @@ class EchoBot(ClientXMPP):
 
     def on_message(self, msg):
         if msg['type'] in ('chat', 'normal'):
+            print(self['xep_0384'].is_encrypted(msg))
             self.process_command(msg)
+
+    async def message(self, msg, allow_untrusted = True):
+
+        mfrom = mto = msg['from']
+        mtype = msg['type']
+
+        try:
+            encrypted = msg['omemo_encrypted']
+            body = self['xep_0384'].decrypt_message(encrypted, mfrom, allow_untrusted)
+            decoded = body.decode('utf8')
+            if self.is_command(decoded):
+                await self.handle_command(mto, mtype, decoded)
+            return None
+        except (MissingOwnKey,):
+            # The message is missing our own key, it was not encrypted for
+            # us, and we can't decrypt it.
+            await self.plain_reply(
+                mto, mtype,
+                'Error: Message not encrypted for me.',
+            )
+            return None
+        except (NoAvailableSession,) as exn:
+            # We received a message from that contained a session that we
+            # don't know about (deleted session storage, etc.). We can't
+            # decrypt the message, and it's going to be lost.
+            # Here, as we need to initiate a new encrypted session, it is
+            # best if we send an encrypted message directly. XXX: Is it
+            # where we talk about self-healing messages?
+            await self.encrypted_reply(
+                mto, mtype,
+                'Error: Message uses an encrypted '
+                'session I don\'t know about.',
+            )
+            return None
+        except (UndecidedException, UntrustedException) as exn:
+            # We received a message from an untrusted device. We can
+            # choose to decrypt the message nonetheless, with the
+            # `allow_untrusted` flag on the `decrypt_message` call, which
+            # we will do here. This is only possible for decryption,
+            # encryption will require us to decide if we trust the device
+            # or not. Clients _should_ indicate that the message was not
+            # trusted, or in undecided state, if they decide to decrypt it
+            # anyway.
+            await self.plain_reply(
+                mto, mtype,
+                "Error: Your device '%s' is not in my trusted devices." % exn.device,
+            )
+            # We resend, setting the `allow_untrusted` parameter to True.
+            await self.message(msg, allow_untrusted=True)
+            return None
+        except (EncryptionPrepareException,):
+            # Slixmpp tried its best, but there were errors it couldn't
+            # resolve. At this point you should have seen other exceptions
+            # and given a chance to resolve them already.
+            await self.plain_reply(mto, mtype, 'Error: I was not able to decrypt the message.')
+            return None
+        except (Exception,) as exn:
+            await self.plain_reply(mto, mtype, 'Error: Exception occured while attempting decryption.\n%r' % exn)
+            raise
+
+        return None
 
     def bind_command(self, command: str, function: types.FunctionType):
         assert command.isalpha(), "command name should be only made of alphabet letters"
@@ -127,5 +194,29 @@ def no_command_found(args):
 
 
 xmpp = EchoBot(JID, PASSWORD)
+# xmpp.register_plugin('xep_0030')  # Service Discovery
+# xmpp.register_plugin('xep_0199')  # XMPP Ping
+xmpp.register_plugin('xep_0380')  # Explicit Message Encryption
+
+DATA_DIR = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)),
+        'omemo',
+    )
+
+os.makedirs(DATA_DIR, exist_ok=True)
+
+try:
+    xmpp.register_plugin(
+        'xep_0384',
+        {
+            'data_dir': DATA_DIR,
+        },
+        module=slixmpp_omemo,
+    )  # OMEMO
+
+except (PluginCouldNotLoad,):
+    print('And error occured when loading the omemo plugin.')
+    sys.exit(1)
+
 xmpp.connect()
 xmpp.process(forever=True)
